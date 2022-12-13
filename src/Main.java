ad7a88c71e8f1829f0433d43850e7610f395aa32
Aleksandr Yakovlev <yak0vlev.as@yandex.ru>
2022-12-13 23:16:42 +0300

    меню первое ветвляние работает + второе работает

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-1
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
@@ -20,16 +20,32 @@ public class Main {
                 String month = months[monthINT - 1];
                 System.out.println("за какой день \"" + month + "\" внести шаги ?");
                 int day = scanner.nextInt();
-                System.out.println("Введите колличество шагов !");
-                int step = scanner.nextInt();
+                while (true){
+                    System.out.println("Введите колличество шагов !");
+                    int step = scanner.nextInt();
+                    if(step < 0){
+                    System.out.println("Неможет быть! Постарайтесь еще.");
+                    }else {
+                        System.out.println("Отлично ! Осталось еще " + (targetStep - step) + " шагов за сегодня. ");
+                        break;
+                    }
+                }
                 // Надо отправить в класс StepTracker в в метод добавления шагов
+            }else if (userInput == 2){
+                System.out.println("За какой месяц хотели бы получить статистику?");
+                System.out.println(" Январь - 1 ... Декабрь - 12 ");
+                int monthINT = scanner.nextInt();
+                String month = months[monthINT - 1];
+                // Надо отправить в класс StepTracker в ~~ 7 методов
+            }else if(userInput == 3){
+                //**********************
+            }else if(userInput == 0){
+                System.out.println("Выход");
+                break;
+            }else {
+                System.out.println("Извините, такой команды пока нет.");
             }
-
-
-
-
         }
-
     }
     public static void printMenu() {
         System.out.println("Что вы хотите сделать? ");
