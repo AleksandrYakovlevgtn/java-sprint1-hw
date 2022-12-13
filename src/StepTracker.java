bcd6e07c2825da140dfd14f82c80018e2a85aa7c
Aleksandr Yakovlev <yak0vlev.as@yandex.ru>
2022-12-13 15:33:40 +0300

    меню первое ветвляние работает

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -3,7 +3,7 @@ public class StepTracker {
     Scanner scanner = new Scanner(System.in);
     int targetSteps = 10_000;
    // int[] days = new int[30]; // обьявил масив с именем -days- и 30 целыми числами
-     MonthData[] monthToData;
+    /* MonthData[] monthToData;
     public StepTracker() {  // Имя "MonthData" масив с месяцем
         monthToData = new MonthData[12];
         for (int i = 0; i < monthToData.length; i++) {
@@ -11,18 +11,18 @@ public class StepTracker {
         }
     }
 
-     class MonthData {
-         /* daysMontch[] daysToMontch;
+       class MonthData {
+         daysMontch[] daysToMontch;
          public DaysDate() { // Имя "daysToMontch" масив для дней
              daysToMontch = daysMontch[30];
              for (int i = 0; i < daysToMontch.length; i++) {
                  daysToMontch[i] = new daysMontch(); */
              }
-         }
-     }
 
 
 
-}
+
+
+
       //    System.out.println("Введите количество шагов: ");
       //    monthToData[month].dayData[day]=scanner.nextInt();
