f8b8702c9dd29c1255694c81d2c3320c7c2d4403
Aleksandr Yakovlev <yak0vlev.as@yandex.ru>
2022-12-13 15:07:32 +0300

    меню

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+        System.out.println("Здравствуйте это приложение \"Счетчик шагов!\"");
+
+    }
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1 - Ввести количество шагов за определённый день месяца");
+        System.out.println("2 - Напечатать статистику за определённый месяц");
+        System.out.println("3 - Изменить цель по количеству шагов в день");
+        System.out.println("0 - Выход");
     }
 }
---
+++ src/StepTracker.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class StepTracker {
+    Scanner scanner = new Scanner(System.in);
+    int targetSteps = 10_000;
+   // int[] days = new int[30]; // обьявил масив с именем -days- и 30 целыми числами
+     MonthData[] monthToData;
+    public StepTracker() {  // Имя "MonthData" масив с месяцем
+        monthToData = new MonthData[12];
+        for (int i = 0; i < monthToData.length; i++) {
+            monthToData[i] = new MonthData();
+        }
+    }
+
+     class MonthData {
+         /* daysMontch[] daysToMontch;
+         public DaysDate() { // Имя "daysToMontch" масив для дней
+             daysToMontch = daysMontch[30];
+             for (int i = 0; i < daysToMontch.length; i++) {
+                 daysToMontch[i] = new daysMontch(); */
+             }
+         }
+     }
+
+
+
+}
+      //    System.out.println("Введите количество шагов: ");
+      //    monthToData[month].dayData[day]=scanner.nextInt();
